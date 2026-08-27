b17e168f756e05e42723ba36abcb7a10670b860b
selvasebastian <selva.sebastian@icloud.com>
2026-08-27 15:07:37 +0200

    Add comments to schema.sql

--- schema.sql
+++ schema.sql
@@ -1,3 +1,4 @@
+-- Current account balance is not saved, but later calculated (opening_balance_cents + sum all transactions)
 CREATE TABLE accounts (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     name VARCHAR NOT NULL,
@@ -5,12 +6,14 @@ CREATE TABLE accounts (
     opening_balance_cents INTEGER NOT NULL DEFAULT 0
 );
 
+-- Categories for Transactions, kind is either income or expense
 CREATE TABLE categories (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     name VARCHAR NOT NULL UNIQUE,
     kind VARCHAR NOT NULL
 );
 
+-- Single Transactions, cent amounts get stored, negative amount = expensive, positive amount = income
 CREATE TABLE transactions (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     transaction_date DATE NOT NULL,
@@ -20,6 +23,7 @@ CREATE TABLE transactions (
     category_id INTEGER NOT NULL REFERENCES categories (id)
 );
 
+-- Budgets per month, category id is not NOT Null because NULL = overall budget for the month
 CREATE TABLE budgets (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     month VARCHAR NOT NULL,
@@ -28,6 +32,7 @@ CREATE TABLE budgets (
     UNIQUE(month, category_id)
 );
 
+-- Indices for faster querys
 CREATE INDEX idx_transactions_date ON transactions(transaction_date);
 CREATE INDEX idx_transactions_account ON transactions(account_id, transaction_date);
 CREATE INDEX idx_transactions_category ON transactions (category_id, transaction_date);
